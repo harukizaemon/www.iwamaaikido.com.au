f8e1e8184001993636a44ee94bf3c818f3ea89f7
Simon Harris <haruki_zaemon@mac.com>
2014-09-21 22:17:37 +1000

    Scrolling for sneaky-peek images

--- js/iwamaaikido.js
+++ js/iwamaaikido.js
@@ -1,9 +1,9 @@
 // Ease the scrolling from the nav menu
 $(function() {
   $(".nav.navbar-nav a").bind("click", function(event) {
-    var $anchor = $(this);
+    var $anchor = $(this).attr("href");
     $("html, body").stop().animate({
-      scrollTop: $($anchor.attr("href")).offset().top
+      scrollTop: $($anchor).offset().top
     }, 500, "easeInOutExpo");
     event.preventDefault();
   });
@@ -12,4 +12,15 @@ $(function() {
 // Close the responsive nav menu on click
 $(".navbar-collapse ul li a").click(function() {
     $(".navbar-toggle:visible").click();
+});
+
+// Ease the scrolling from sneaky-peek images
+$(function() {
+  $(".sneaky-peek img").bind("click", function(event) {
+    var $image = $(this);
+    $("html, body").stop().animate({
+      scrollTop: $($image).offset().top
+    }, 500, "easeInOutExpo");
+    event.preventDefault();
+  });
 });
