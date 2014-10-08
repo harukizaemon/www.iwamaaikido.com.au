76281e4021b8a063eb253416254ba996a28e28d5
Simon Harris <haruki_zaemon@mac.com>
2014-10-08 18:32:01 +1100

    Basic content is in

--- js/iwamaaikido.js
+++ js/iwamaaikido.js
@@ -13,14 +13,3 @@ $(function() {
 $(".navbar-collapse ul li a").click(function() {
     $(".navbar-toggle:visible").click();
 });
-
-// Ease the scrolling from sneaky-peek images
-$(function() {
-  $(".sneaky-peek img").bind("click", function(event) {
-    var $image = $(this);
-    $("html, body").stop().animate({
-      scrollTop: $($image).offset().top
-    }, 500, "easeInOutExpo");
-    event.preventDefault();
-  });
-});
